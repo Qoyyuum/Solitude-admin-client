f066d03b91eef528526d0369c5cf38f64bc8af54
yash9263 <yashwant8530@gmail.com>
2021-02-08 21:04:53 +0530

    added sign up page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import SignIn from './components/SignIn';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-        <SignIn/></div>
+        {/* <SignIn/> */}
+        <SignUp />
+        {/* <Login/> */}
+        </div>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAIccGIIVBGgPcWmOFp7OhwOWq2rUSOXlc",
+    authDomain: "solitude-admin-client.firebaseapp.com",
+    projectId: "solitude-admin-client",
+    storageBucket: "solitude-admin-client.appspot.com",
+    messagingSenderId: "74737409636",
+    appId: "1:74737409636:web:8f1352a5217f1413768971",
+    measurementId: "G-6SBZQDXKCM"
+  };
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
