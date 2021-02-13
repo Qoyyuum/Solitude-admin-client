d3c3ec8f1b37cfe29dde58a47d204cf3b273be34
yash9263 <yashwant8530@gmail.com>
2021-02-13 23:37:22 +0530

    added appbar

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,15 +1,17 @@
 import React, {useContext} from "react";
 import {UserContext} from "../providers/UserProvider";
 import {auth} from "../firebase";
+import ButtonAppBar from "./ButtonAppBar";
 
 export default function ProfilePage() {
     const user = useContext(UserContext);
     const {displayName, email} = user;
     return (
         <div>
+            <ButtonAppBar />
             <h2>{displayName}</h2>
             <h3>{email}</h3>
-            <button onClick={() => {auth.signOut()}}>Sign Out</button>
+            
         </div>
     );
 }
